4b80f7a3c19b476f975d6178366762b129b4fd1a
5n0weirdo <snoweirdo@protonmail.com>
2021-11-14 19:52:07 +0200

    Changed json

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   titles: {
-    marginBottom: "125%",
+    marginBottom: "130%",
     alignItems: "center",
     width: "100%",
     height: "100%",
