d9ae0d527f8752c521013cde85b3a3430a596287
5n0weirdo <snoweirdo@protonmail.com>
2021-11-14 18:51:11 +0200

    Added header and subheading

--- App.js
+++ App.js
@@ -5,7 +5,12 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.carContainer}></View>
+      <View>
+        <Text>Model S</Text>
+        <Text>Starting at $69,420</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +23,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  carContainer: {},
 });
